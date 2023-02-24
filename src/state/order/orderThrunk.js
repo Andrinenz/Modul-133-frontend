5af60d43793dd05756506ed2d9942972e3a3e6f2
Andrin Enz <89848517+Andrinzh7@users.noreply.github.com>
2023-02-24 17:54:13 +0100

    Update orderThrunk.js

--- src/state/order/orderThrunk.js
+++ src/state/order/orderThrunk.js
@@ -6,6 +6,51 @@
 /* orderThrunk                                                                */
 /*----------------------------------------------------------------------------*/
 
+export const fetchOrdersData = () => {
+  return async (dispatch) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const fetchOrderById = () => {
+  return async (dispatch) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const fetchCreateOrder = () => {
+  return async (dispatch) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const fetchUpdateOrder = () => {
+  return async (dispatch) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const fetchDeleteOrder = () => {
+  return async (dispatch) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 /*----------------------------------------------------------------------------*/
 /* EXPORTS                                                                    */
 /*----------------------------------------------------------------------------*/
