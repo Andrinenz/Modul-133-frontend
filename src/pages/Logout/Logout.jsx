dda79433c97c9ceaa4fccf67a430affe6f3613aa
Andrin Enz <89848517+Andrinzh7@users.noreply.github.com>
2023-03-14 20:36:43 +0100

    done

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -2,14 +2,30 @@
 /* IMPORTS                                                                    */
 /*----------------------------------------------------------------------------*/
 
+import { GroupAccess } from '@carbon/icons-react';
+import { useNavigate } from 'react-router';
+import './Logout.scss';
+
 /*----------------------------------------------------------------------------*/
 /* Logout                                                                     */
 /*----------------------------------------------------------------------------*/
 
 const Logout = () => {
+  let navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Logout</h1>
+    <div className='title-wrapper center d-flex fd-c logout-wrapper'>
+      <GroupAccess size='32' className='logoutSiteIcon' />
+      <h1 className='center'>You are now logged out</h1>
+      <h4>Thanks for visiting our webshop</h4>
+
+      <p
+        className='cursor-pointer decoration-underline'
+        style={{ color: '#0f62fe' }}
+        onClick={(e) => navigate('/login')}
+      >
+        Login
+      </p>
     </div>
   );
 };
