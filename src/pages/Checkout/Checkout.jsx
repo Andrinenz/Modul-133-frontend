f96d9b8eee4dd889add234f7477abe3d38375704
Andrin Enz <89848517+Andrinzh7@users.noreply.github.com>
2023-03-15 22:39:42 +0100

    still working on

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,6 +2,7 @@
 /* IMPORTS                                                                    */
 /*----------------------------------------------------------------------------*/
 
+import { Input } from 'antd';
 import ProductItem from './assets/ProductItem';
 import './Checkout.scss';
 
@@ -11,26 +12,45 @@ import './Checkout.scss';
 
 const Checkout = () => {
   return (
-    <div className='d-flex bxw-wrap'>
-      <div className='d-flex fd-c cds--col-lg-11 pl-0 pr-0'>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
-        <h1>Test 2</h1>
+    <>
+      <div className='bx-wrap'>
+        <div className='d-flex res-top fd-c cds--col-lg-11 pl-0 pr-0'>
+          <div className='bcol-ibm-gray-20 mr-3 pt-2 pl-2 pr-2 ml-3 mt-2'>
+            <h1 className='mb-0'>Checkout</h1>
+            <div className='border-bottom mt-1' />
+            <div>
+              <h3 className='mb-0 mt-2'>Personal details:</h3>
+              <div className='d-flex f-jb mt-2'>
+                <div className='cds--col-lg-7 mr-1 pl-0 pr-0'>
+                  <h5>Firstname:</h5>
+                  <Input size='large' placeholder='firstname' />
+                </div>
+                <div className='cds--col-lg-7 ml-1 pl-0 pr-0'>
+                  <h5>Lastname:</h5>
+                  <Input size='large' placeholder='lastname' />
+                </div>
+              </div>
+              <div className='cds--col-lg-7 pl-0 pr-0 mt-1 mr-1'>
+                <h5>Email:</h5>
+                <Input size='large' placeholder='email' />
+              </div>
+            </div>
+          </div>
+          <div className='cds--col-lg-11 pl-0 pr-0 total-box pt-2'>
+            <div className='d-flex f-ac f-jb'>
+              <h2 className='text-bold pl-3'>Total</h2>
+              <h2 className=''>300.-</h2>
+            </div>
+          </div>
+        </div>
+        <div className='d-flex fd-c cds--col-lg-5 pl-0 pr-0 fixed-sidenav bcol-ibm-white'>
+          <div className='d-flex title-box border-bottom f-jc f-ac mt-2'>
+            <h2 className='text-bold'>Item List</h2>
+          </div>
+          <ProductItem />
+        </div>
       </div>
-      <div className='d-flex cds--col-lg-5 pl-0 pr-0 fixed-sidenav bcol-ibm-white'>
-        <ProductItem />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Checkout/assets/ProductItem.jsx
+++ src/pages/Checkout/assets/ProductItem.jsx
@@ -2,6 +2,9 @@
 /* IMPORTS                                                                    */
 /*----------------------------------------------------------------------------*/
 
+import { ArrowRight } from '@carbon/icons-react';
+import { Link } from '@carbon/react';
+
 /*----------------------------------------------------------------------------*/
 /* ProductItem                                                                */
 /*----------------------------------------------------------------------------*/
@@ -10,8 +13,45 @@ const ProductItem = (props) => {
   let data = props.data;
 
   return (
-    <div className=''>
-      <h1>Test</h1>
+    <div className='item-box ml-1 mr-1 bcol-ibm-gray-30 pl-2 pt-2 pr-2 d-flex fd-c mt-2 mb-2'>
+      <div>
+        <h3 className='text-bold'>Hoodie Basic Print</h3>
+      </div>
+      <div className='border-bottom' />
+      <div className='mt-2 d-flex'>
+        <div>
+          <h4 className='text-bold'>Size:</h4>
+        </div>
+        <div>
+          <h4 className='ml-1'>M</h4>
+        </div>
+      </div>
+      <div className='d-flex'>
+        <div>
+          <h4 className='text-bold'>Item Count:</h4>
+        </div>
+        <div>
+          <h4 className='ml-1'>3</h4>
+        </div>
+      </div>
+      <div className='d-flex f-ac f-jb mb-1 mt-2'>
+        <Link size='lg'>
+          <a
+            href='products'
+            rel='noreferrer'
+            target={'_blank'}
+            className='deco-none'
+          >
+            To Item
+          </a>
+        </Link>
+        <ArrowRight className='col-ibm-blue-60 ml-2' size={'20'} />
+      </div>
+      <div className='border-bottom' />
+      <div className='d-flex mt-1 f-jb'>
+        <h3 className='text-bold'>Price:</h3>
+        <h3>79.-</h3>
+      </div>
     </div>
   );
 };
