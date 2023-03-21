80f5eb563b2203a81787ed13a33925965ad25c24
Noah <noahsch222@gmail.com>
2023-03-21 13:15:58 +0100

    Updates

--- src/pages/Products/assets/Card.jsx
+++ src/pages/Products/assets/Card.jsx
@@ -12,9 +12,29 @@ const Card = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="Cards">
-      <div>
-        <h1 className="">Card test</h1>
+    <div className="BackGround">
+      <div className="ProductTitle">
+        <h1>Products</h1>
+      </div>
+      <div className="Cards">
+        <div>
+          <h1 className="Card1">Card test</h1>
+        </div>
+        <div>
+          <h1 className="Card2">Card test2</h1>
+        </div>
+        <div>
+          <h1 className="Card3">Card test2</h1>
+        </div>
+        <div>
+          <h1 className="Card4">Card test2</h1>
+        </div>
+        <div>
+          <h1 className="Card5">Card test2</h1>
+        </div>
+        <div>
+          <h1 className="Card6">Card test2</h1>
+        </div>
       </div>
     </div>
   );
