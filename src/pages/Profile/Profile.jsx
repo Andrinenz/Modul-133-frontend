c331be543523d75a25d5a0eedb8ddb0d00ef1a69
Andrin Enz <89848517+Andrinzh7@users.noreply.github.com>
2023-04-01 23:24:16 +0200

    started

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,15 +2,45 @@
 /* IMPORTS                                                                    */
 /*----------------------------------------------------------------------------*/
 
+import { useEffect } from 'react';
+import { Loading, Tile } from '@carbon/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { getUser } from '../../state/user/userSelector';
+import { fetchUserProfile } from '../../state/user/userThrunk';
+import FrameImg from './img/profile.png';
+import './Profile.scss';
+
 /*----------------------------------------------------------------------------*/
 /* Profile                                                                    */
 /*----------------------------------------------------------------------------*/
 
 const Profile = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { profile, profileLoaded } = useSelector(getUser);
+
+  useEffect(() => {
+    dispatch(fetchUserProfile(id));
+  }, [dispatch, id]);
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <>
+      {profileLoaded ? (
+        <div className='bx-wrap f-jc'>
+          <div className='cds--col-lg-13 cds--col-md-7 cds--col-sm-4 pt-3'>
+            <Tile className='mt-3 pl-0 pt-0 pr-0 pb-3'>
+              <div className='w100p mt-2'>
+                <img src={FrameImg} alt='Main' className='frame-img w100p' />
+              </div>
+              <div className='bx-wrap mt-4'></div>
+            </Tile>
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
--- src/state/user/userThrunk.js
+++ src/state/user/userThrunk.js
@@ -3,16 +3,19 @@
 /*----------------------------------------------------------------------------*/
 
 import {
+  getProfile,
   logoutUser,
   setAccessToken,
   setButtonLoading,
+  setProfile,
   setUser,
-} from "./userSlice";
-import { addErrorNotification } from "../notification/notificationSlice.js";
-import validator from "validator";
-import jwtDecode from "jwt-decode";
-import axios from "axios";
-import { axiosAuth } from "../../helpers/axios";
+} from './userSlice';
+import { addErrorNotification } from '../notification/notificationSlice.js';
+import validator from 'validator';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import { axiosAuth } from '../../helpers/axios';
+import { config } from '../../helpers/config';
 
 /*----------------------------------------------------------------------------*/
 /* userThrunk                                                                 */
@@ -21,7 +24,7 @@ const extractToken = async (email, password, start, navigate, dispatch) => {
   if (!start) {
     dispatch(setButtonLoading(true));
     let res = await axios
-      .post("http://localhost:8080/api/auth/login", {
+      .post('http://localhost:8080/api/auth/login', {
         email: email,
         password: password,
       })
@@ -29,8 +32,8 @@ const extractToken = async (email, password, start, navigate, dispatch) => {
         if (error.response.status === 401) {
           dispatch(
             addErrorNotification({
-              message: "Error",
-              description: "Wrong Email or password entered",
+              message: 'Error',
+              description: 'Wrong Email or password entered',
             })
           );
           dispatch(setButtonLoading(false));
@@ -41,7 +44,7 @@ const extractToken = async (email, password, start, navigate, dispatch) => {
       let token = res.data.token;
 
       if (token !== null && validator.isJWT(token)) {
-        localStorage.setItem("accessToken", token);
+        localStorage.setItem('accessToken', token);
         let decodedUser = jwtDecode(token);
         return { valid: true, token: token, user: decodedUser };
       }
@@ -51,10 +54,10 @@ const extractToken = async (email, password, start, navigate, dispatch) => {
     }
     dispatch(setButtonLoading(false));
   } else {
-    let localStorageToken = localStorage.getItem("accessToken");
+    let localStorageToken = localStorage.getItem('accessToken');
     if (
       localStorageToken !== null &&
-      validator.isJWT(localStorage.getItem("accessToken"))
+      validator.isJWT(localStorage.getItem('accessToken'))
     ) {
       let decodedUser = jwtDecode(localStorageToken);
       if (
@@ -63,7 +66,7 @@ const extractToken = async (email, password, start, navigate, dispatch) => {
       ) {
         return { valid: true, token: localStorageToken, user: decodedUser };
       }
-      console.log("token abgelaufen", localStorageToken);
+      console.log('token abgelaufen', localStorageToken);
       dispatch(setButtonLoading(false));
       return { valid: false, token: null, user: null };
     } else {
@@ -85,7 +88,7 @@ export const login = (email, password, start, navigate, dispatch2) => {
     if (data.valid) {
       dispatch(setAccessToken(data.token));
       dispatch(setUser(data.user));
-      dispatch(navigate("/products"));
+      dispatch(navigate('/products'));
     }
   };
 };
@@ -93,30 +96,47 @@ export const login = (email, password, start, navigate, dispatch2) => {
 export const fetchCreateUser = (obj, navigate) => {
   return async (dispatch) => {
     try {
-      let res = await axiosAuth.post("/api/auth/createUser", obj);
+      let res = await axiosAuth.post('/api/auth/createUser', obj);
 
       if (res.status === 201) {
         if (res.data.token) {
           let token = res.data.token;
 
           if (token !== null && validator.isJWT(token)) {
-            localStorage.setItem("accessToken", token);
+            localStorage.setItem('accessToken', token);
             let decodedUser = jwtDecode(token);
             dispatch(logoutUser());
             dispatch(setUser(decodedUser));
             dispatch(setAccessToken(res.data.token));
-            dispatch(navigate("/products"));
+            dispatch(navigate('/products'));
           }
         } else {
           dispatch(
             addErrorNotification({
-              message: "Error",
-              description: "Failed while creating user",
+              message: 'Error',
+              description: 'Failed while creating user',
             })
           );
         }
       }
     } catch (err) {}
+  };
+};
+
+export const fetchUserProfile = (id) => {
+  return async (dispatch) => {
+    dispatch(getProfile());
+    try {
+      const res = await axiosAuth.get(`/api/user/getUserById?id=${id}`);
+      dispatch(setProfile(res.data.result));
+    } catch (err) {
+      dispatch(
+        addErrorNotification({
+          message: 'Error',
+          description: err.toString(),
+        })
+      );
+    }
   };
 };
 
